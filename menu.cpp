099f99b09c9de009e8fdbe838ff52c7289fff39a
Arliey <xrley12@gmail.com>
2025-12-29 15:21:22 +0700

    update

--- menu.cpp
+++ menu.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void tampilMenu() {
     cout << "\n=== SISTEM PERPUSTAKAAN ===\n";
     cout << "1. Login Admin\n";
-    cout << "2. Login Member\n";
+    cout << "2. Masukan ID Member Untuk Meminjam\n";
     cout << "3. Daftar Member\n";
     cout << "0. Keluar\n";
 }
